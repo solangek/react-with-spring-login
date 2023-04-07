6bbae9f099b60979d8782dd7749f4ff66d7f8167
Sol <sodanabe@gmail.com>
2023-04-07 18:06:45 +0300

    2023

--- src/App.js
+++ src/App.js
@@ -9,13 +9,18 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          <form action={"/logout"} method="GET">
+            <button type="submit">Logout</button>
+          </form>
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn
         </a>
       </header>
     </div>
